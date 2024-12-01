aec506520a8aef019cb80e5bb0dd4bbf7c81ca1e
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-12-01 09:56:18 +0100

    finished day 1

--- src/day1/main.ts
+++ src/day1/main.ts
@@ -27,4 +27,32 @@ for (let i = 0; i < list1.length; i++) {
   distanceSum += distance;
 }
 
-console.log(distanceSum);
+console.log(distanceSum); //2430334
+
+// DAY 1 - TASK 2
+
+// Step 1: Count occurences of each number in list2 (to only go through this list once)
+const numCountList2 = new Map<number, number>();
+
+for (const num of list2) {
+  const oldNumCount = numCountList2.get(num);
+  if (oldNumCount === undefined) {
+    numCountList2.set(num, 1);
+  } else {
+    numCountList2.set(num, oldNumCount + 1);
+  }
+}
+
+let similarityScore = 0;
+
+// Step 2: Go through list 1 and check how often the number occurs in list 2,
+// then multiply the number with the occurence and add it to the sum
+
+for (const num of list1) {
+  const occurence = numCountList2.get(num);
+  if (occurence !== undefined) {
+    similarityScore += num * occurence;
+  }
+}
+
+console.log(similarityScore); // 28786472
