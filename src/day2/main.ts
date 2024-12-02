485bdbecffdc751dc44c76a835dd20248c4cc2e5
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-12-02 21:19:27 +0100

    restructure day 2 again!

--- src/day2/main.ts
+++ src/day2/main.ts
@@ -4,100 +4,114 @@ const inputText = await Deno.readTextFile("src/day2/input.txt");
 const reports = inputText.split("\n");
 reports.sort();
 
-type ReportState = {
-  // per-report state
-  all_increasing: boolean;
-  all_decreasing: boolean;
-  has_previous_failure: boolean;
-
-  // per-level state
-  previous_level: number;
-  level: number;
-  is_failure: boolean;
-};
-
-function isLevelValid(state: ReportState): ReportState {
-  if (state.previous_level < state.level) {
-    state.all_decreasing = false;
+function isReportValid(report: number[]) {
+  if (report.length < 2) {
+    console.error(`Report must have at least 2 levels! `, report);
+    return {
+      report,
+      valid: false,
+      reason: "Report must have at least 2 levels!",
+    };
   }
 
-  if (state.previous_level > state.level) {
-    state.all_increasing = false;
+  let initial_direction: undefined | "increasing" | "decreasing" = undefined;
+
+  for (let i = 1; i < report.length; i++) {
+    const previous_level = report[i - 1];
+    const level = report[i];
+
+    // init previous_direction
+    if (initial_direction === undefined) {
+      if (previous_level < level) {
+        initial_direction = "increasing";
+      } else {
+        initial_direction = "decreasing";
+      }
+    } else {
+      // compare with previous_direction
+      if (initial_direction === "increasing" && previous_level > level) {
+        return {
+          report,
+          valid: false,
+          reason_code: "direction_missmatch",
+          reason:
+            `initial_direction is increasing, but prev_level ${previous_level} is bigger than curr_level ${level}`,
+        };
+      }
+
+      if (initial_direction === "decreasing" && previous_level < level) {
+        return {
+          report,
+          valid: false,
+          reason_code: "direction_missmatch",
+          reason:
+            `initial_direction is decreasing, but prev_level ${previous_level} is smaller than curr_level ${level}`,
+        };
+      }
+    }
+
+    // check diff
+    const diff = Math.abs(previous_level - level);
+    if (diff < 1 || diff > 3) {
+      return {
+        report,
+        valid: false,
+        reason_code: "step_size_invalid",
+        reason:
+          `diff between prev_level ${previous_level} and curr_level ${level} is ${diff} (must be <=1 and <=3)`,
+      };
+    }
   }
 
-  if (!state.all_decreasing && !state.all_increasing) {
-    // Shortcut: first condition not met for this report
-    state.is_failure = true;
-  }
-
-  const diff = Math.abs(state.previous_level - state.level);
-  if (diff < 1 || diff > 3) {
-    // Shortcut: second condition not met for this report
-    state.is_failure = true;
-  }
-
-  return state;
+  return { report, valid: true };
 }
 
 const validatedReports = reports.map((report) => {
   const levelStrings = report.split(" ");
+  const levels = levelStrings.map((num) => Number.parseInt(num));
 
-  let reportState = {
-    all_increasing: true as boolean,
-    all_decreasing: true as boolean,
-    has_previous_failure: false as boolean,
-    previous_level: 0 as number,
-    level: 0 as number,
-    is_failure: false as boolean,
-  } satisfies ReportState;
+  const result = isReportValid(levels);
 
-  for (let i = 1; i < levelStrings.length; i++) {
-    reportState.previous_level = Number.parseInt(levelStrings[i - 1]);
-    reportState.level = Number.parseInt(levelStrings[i]);
-    reportState.is_failure = false;
+  return result;
 
-    const newState = isLevelValid(reportState);
+  // for (let i = 1; i < levelStrings.length; i++) {
 
-    // PROBLEM DAMPENER
+  // PROBLEM DAMPENER
 
-    if (newState.is_failure && reportState.has_previous_failure) {
-      // Shortcut: if the report has already failed,
-      // and this level fails, then the report is invalid
-      return { report, valid: false };
-    }
+  // if (newState.is_failure && reportState.has_previous_failure) {
+  //   // Shortcut: if the report has already failed,
+  //   // and this level fails, then the report is invalid
+  //   return { report, valid: false };
+  // }
 
-    if (newState.is_failure && !reportState.has_previous_failure) {
-      // check if report would be valid if we skip this failing level
+  // if (newState.is_failure && !reportState.has_previous_failure) {
+  //   // check if report would be valid if we skip this failing level
 
-      // Shortcut: if this level is the last level, then the report will be valid when this is skipped!
-      if (i === levelStrings.length - 1) {
-        return { report, valid: true };
-      }
+  //   // Shortcut: if this level is the last level, then the report will be valid when this is skipped!
+  //   if (i === levelStrings.length - 1) {
+  //     return { report, valid: true };
+  //   }
 
-      // aka: do all checks for this level again, but comparing: previous_level and levelStrings[i+1]
-      const newState2 = isLevelValid({
-        ...reportState,
-        level: Number.parseInt(levelStrings[i + 1]),
-        is_failure: false,
-        has_previous_failure: true,
-      });
+  //   // aka: do all checks for this level again, but comparing: previous_level and levelStrings[i+1]
+  //   const newState2 = isLevelValid({
+  //     ...reportState,
+  //     level: Number.parseInt(levelStrings[i + 1]),
+  //     is_failure: false,
+  //     has_previous_failure: true,
+  //   });
 
-      if (newState2.is_failure) {
-        // if the next level is also a failure, then the report is invalid
-        return { report, valid: false };
-      } else {
-        // if the next level is valid, then we can skip this level
-        reportState = newState2;
-        i = i + 1;
-        continue;
-      }
-    }
+  //   if (newState2.is_failure) {
+  //     // if the next level is also a failure, then the report is invalid
+  //     return { report, valid: false };
+  //   } else {
+  //     // if the next level is valid, then we can skip this level
+  //     reportState = newState2;
+  //     i = i + 1;
+  //     continue;
+  //   }
+  // }
 
-    // else: continue to check next level
-    reportState = newState;
-  }
-
-  return { report, valid: true };
+  // }
 });
 
 const safeReports = validatedReports.filter((r) => r.valid === true);
