140d88b8b158c627c131da18dfe4ef57083edbe1
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-12-21 22:15:28 +0100

    correctly parse input data

--- src/day5-1/main.ts
+++ src/day5-1/main.ts
@@ -11,7 +11,35 @@ const inputText = await Deno.readTextFile(
   //   join(import.meta.dirname, "input.txt"),
 );
 
-const [section1, section2] = inputText.split("\n\n");
+const [ruleLines, updateLines] = inputText.split("\n\n");
 
-console.log(section1);
-console.log(section2);
+/**
+ * Rules:
+ * 1. Given X | Y, Page Nr X must be printed before page Nr Y
+ */
+
+// Step 1: Produce an index for the rules, aka:
+// Page to print -> Pages that must be printed before
+const printRules = new Map<number, Set<number>>();
+
+for (const ruleLine of ruleLines.split("\n")) {
+  const [before, after] = ruleLine.split("|").map((x) => parseInt(x, 10));
+  if (!printRules.has(after)) {
+    printRules.set(after, new Set());
+  }
+  printRules.get(after)!.add(before);
+}
+
+console.log(printRules);
+
+// Step 2: Produce a list of updates
+const updates = updateLines.split("\n").map((line) => line.split(","));
+console.log(updates);
+
+// Step 3: A function for checking if one update is valid
+function isUpdateValid(update: number[]) {
+  const pagesPrintedBefore: number[] = [];
+
+  for (const page of update) {
+  }
+}
