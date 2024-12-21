ba5761326f322c3ad7e3455fd0a37f59804554f3
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-12-21 21:59:54 +0100

    read demo input sections

--- src/day5-1/main.ts
+++ src/day5-1/main.ts
@@ -10,3 +10,8 @@ const inputText = await Deno.readTextFile(
   join(import.meta.dirname, "input_demo.txt"),
   //   join(import.meta.dirname, "input.txt"),
 );
+
+const [section1, section2] = inputText.split("\n\n");
+
+console.log(section1);
+console.log(section2);
